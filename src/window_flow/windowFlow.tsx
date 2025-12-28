5c325a09d21ba271db92132a68b2fe451e812cdb
redlybastian <red233786@gmail.com>
2025-12-28 23:02:28 +0530

    set the files remove dark mode

---
+++ src/window_flow/windowFlow.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react"
+import { ReactFlow } from "@xyflow/react"
+
+
+
+export const WindowFlow = ()=>{
+    return(
+        <Box height={'90vh'} width={'90vw'} border={'1px solid black'}>
+            <ReactFlow>
+
+            </ReactFlow>
+        </Box>
+    )
+}
